89544cf2ce1fc909e33d75370a6c09f39721e186
Максим Мамонтов <maksimmamontov@MacBook-Pro-Maksim.local>
2023-01-29 14:01:32 +0300

    Add config mod

---
+++ internal/app/app.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mxmntv/anti_bruteforce/config/config"
+	"github.com/mxmntv/anti_bruteforce/pkg/logger"
+)
+
+func Run(cfg *config.Config) error {
+	logger := logger.New(cfg.Log.Level)
+
+	// mux := http.NewServeMux()
+	// handle := handler.NewEventHandler(eventUseCase, logger)
+	// handle.Register(mux)
+
+	// server := httpserver.NewServer(cfg.HTTP.Host, cfg.HTTP.Port, mux)
+
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		if err := server.Stop(ctx); err != nil {
+			logger.Error("failed to stop http server: " + err.Error())
+		}
+	}()
+
+	logger.Info("calendar is running...")
+
+	if err := server.Start(ctx); err != nil {
+		logger.Error("failed to start http server: " + err.Error())
+		cancel()
+		return err
+	}
+	return nil
+}
